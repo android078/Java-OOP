cf8a01b659be747088e68ca65b36fe6cd8341a1d
Dmitrij <fazzy07@tut.by>
2023-07-14 22:13:29 +0300

    Создание LinkedLista

---
+++ HW_4_LinkedList/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(5);
+        myLinkedList.add(7);
+        myLinkedList.add(12);
+
+        System.out.println(myLinkedList);
+        System.out.println(myLinkedList.get(0));
+        System.out.println(myLinkedList.get(1));
+        System.out.println(myLinkedList.get(2));
+        System.out.println(myLinkedList.get(3));
+
+        System.out.println(myLinkedList.size() + " - размер списка.");
+    }
+}
